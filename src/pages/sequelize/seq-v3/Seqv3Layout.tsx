7fd21fa6a4412e1613b815e80c307b8070b43bab
mkhotamirais <mkhotamirais@gmail.com>
2025-02-12 07:58:35 +0700

    err1

--- src/pages/sequelize/seq-v3/Seqv3Layout.tsx
+++ src/pages/sequelize/seq-v3/Seqv3Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import DynamicHead from "../../../components/DynamicHead";
 import Layout from "../../../components/Layout";
 import { Link, Outlet } from "react-router";
